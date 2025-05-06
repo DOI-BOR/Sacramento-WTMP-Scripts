951fe7025dd81ac1b07099662eb093637fe34b29
Ben <benjamin.saenz@rmanet.com>
2025-05-06 14:39:44 -0700

    Target temps included in results, forecast script fix, new model alternative with W2 Shasta and the rest ResSim
    Forecast target temperatures are available (for reports) with W2 Shasta and ResSim F-parts for comparison in plotting. The forecast scripting was fixed so that tributary temperatures are correct. There is a new WAT model alternative that included Shasta W2, with the rest of the Scramento-Trinity system modeled by ResSim.

---
+++ Post-Process_ResSim_SacTrnRv.py
@@ -0,0 +1,39 @@
+import sys,os
+print(sys.path)
+from hec.heclib.dss import HecDss
+
+from com.rma.model import Project
+sys.path.append(os.path.join(Project.getCurrentProject().getWorkspacePath(), "scripts"))
+
+import DSS_Tools
+reload(DSS_Tools)
+
+model_recs = [ 'alt_model_output1','alt_model_output2'] # linked to records from upstream simulation models
+copy_rec = ['target_temp'] # Linked to a DSS record in DSS file, not a model result
+    
+def computeAlternative(currentAlternative, computeOptions):
+    currentAlternative.addComputeMessage("Computing ScriptingAlternative:" + currentAlternative.getName() )
+   
+    dss_file = computeOptions.getDssFilename()  
+    
+    locations_obj = currentAlternative.getInputDataLocations()
+    copy_obj = DSS_Tools.organizeLocations(currentAlternative, locations_obj, copy_rec, return_dss_paths=False)
+    copy_dss_rec,copy_dss_filepath = DSS_Tools.getDataLocationDSSInfo(copy_obj[0], currentAlternative, computeOptions)
+    print(copy_dss_filepath,copy_dss_rec)    
+
+    # get the f-part from the linked model record, copy the copy_rec to model-fpart in the simulation DSS
+    for model in model_recs:
+        model_obj = DSS_Tools.organizeLocations(currentAlternative, locations_obj, [model], return_dss_paths=False)
+        dss_model_path, _ = DSS_Tools.getDataLocationDSSInfo(model_obj[0], currentAlternative, computeOptions)
+        model_fpart = dss_model_path.split('/')[6]
+        print('model_fpart: ',model_fpart)
+        DSS_Tools.copy_dss_ts(copy_dss_rec,
+                              new_fpart=model_fpart,
+                              dss_file_path=copy_dss_filepath,
+                              dss_file_alt_outpath=dss_file)
+ 
+    return True
+
+
+            
+
--- Pre-Process_ResSim_SacTrn.py
+++ Pre-Process_ResSim_SacTrn.py
@@ -20,11 +20,41 @@ sys.path.append(os.path.join(Project.getCurrentProject().getWorkspacePath(), "sc
 import Forecast_preprocess as fpp
 reload(fpp)
 
+import DSS_Tools
+reload(DSS_Tools)
+
+model_output_and_target = [ 'model_output','target_temp']
+
+
 def computeAlternative(currentAlternative, computeOptions):
     currentAlternative.addComputeMessage("Computing ScriptingAlternative:" + currentAlternative.getName())
     currentAlternative.addComputeMessage('\n')
 
     data_preprocess = fpp.forecast_data_preprocess_ResSim_5Res(currentAlternative, computeOptions)
 
+    # check if  W2 is a part of this simulation, and if so copy the target temperature to the linked record f-part
+    # ------------------------------------------------------------------------------------------------------------------
+    #scripting_run_dir = computeOptions.getRunDirectory()
+    #run_dir,_ = os.path.split(scripting_run_dir)
+    #_,run_name = os.path.split(run_dir)
+    #print('run_name: ',run_name)
+    #print('scripting_run_dir: ',scripting_run_dir)
+    #if 'w2' in run_name.lower():
+    #    currentAlternative.addComputeMessage("  'W2' found in run name, attempting to copy forecast target temperature to F-part of linked model record...")
+    #    dss_file = computeOptions.getDssFilename()   
+    #    locations_obj = currentAlternative.getInputDataLocations()
+    #    locations_obj_2 = DSS_Tools.organizeLocations(currentAlternative, locations_obj, model_output_and_target, return_dss_paths=False)
+    #    # locations_obj_2[0] is a linked output record from a model in the simulation
+    #    # locations_obj_2[1] is a linked dss record, from a DSS file, NOT a model record from the simulation
+    #    dss_path_tt,dss_file_tt = DSS_Tools.getDataLocationDSSInfo(locations_obj_2[1], currentAlternative, computeOptions)
+    #    print(dss_file_tt,dss_path_tt)
+    #    dss_model_path, _ = DSS_Tools.getDataLocationDSSInfo(locations_obj_2[0], currentAlternative, computeOptions)
+    #    model_fpart = dss_model_path.split('/')[6]
+    #    print('model_fpart: ',model_fpart)
+    #    DSS_Tools.copy_dss_ts(dss_path_tt,
+    #                          new_fpart=model_fpart,
+    #                          dss_file_path=dss_file_tt,
+    #                          dss_file_alt_outpath=dss_file)
+
     if data_preprocess: #and acc_dep:
-    	return True
+        return True
