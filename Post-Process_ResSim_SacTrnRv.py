e5bc4d3ae3e0b85947ff9cc9765538dd0783dbc4
bsaenz <BenSae4598@geiconsultants.com>
2025-09-28 17:44:41 -0700

    W2 5.0 support, Shasta 5.0 simulations, CCR->Shasta upstream regression for W2, W2 _provided shapefiles

--- Post-Process_ResSim_SacTrnRv.py
+++ Post-Process_ResSim_SacTrnRv.py
@@ -9,7 +9,7 @@ import DSS_Tools
 reload(DSS_Tools)
 
 model_recs = [ 'alt_model_output1','alt_model_output2'] # linked to records from upstream simulation models
-copy_rec = ['target_temp'] # Linked to a DSS record in DSS file, not a model result
+copy_rec = ['target_temp','target_temp_upstream'] # Linked to a DSS record in DSS file, not a model result
     
 def computeAlternative(currentAlternative, computeOptions):
     currentAlternative.addComputeMessage("Computing ScriptingAlternative:" + currentAlternative.getName() )
@@ -18,19 +18,20 @@ def computeAlternative(currentAlternative, computeOptions):
     
     locations_obj = currentAlternative.getInputDataLocations()
     copy_obj = DSS_Tools.organizeLocations(currentAlternative, locations_obj, copy_rec, return_dss_paths=False)
-    copy_dss_rec,copy_dss_filepath = DSS_Tools.getDataLocationDSSInfo(copy_obj[0], currentAlternative, computeOptions)
-    print(copy_dss_filepath,copy_dss_rec)    
-
-    # get the f-part from the linked model record, copy the copy_rec to model-fpart in the simulation DSS
-    for model in model_recs:
-        model_obj = DSS_Tools.organizeLocations(currentAlternative, locations_obj, [model], return_dss_paths=False)
-        dss_model_path, _ = DSS_Tools.getDataLocationDSSInfo(model_obj[0], currentAlternative, computeOptions)
-        model_fpart = dss_model_path.split('/')[6]
-        print('model_fpart: ',model_fpart)
-        DSS_Tools.copy_dss_ts(copy_dss_rec,
-                              new_fpart=model_fpart,
-                              dss_file_path=copy_dss_filepath,
-                              dss_file_alt_outpath=dss_file)
+    for co in copy_obj:
+        copy_dss_rec,copy_dss_filepath = DSS_Tools.getDataLocationDSSInfo(co, currentAlternative, computeOptions)
+        print(copy_dss_filepath,copy_dss_rec)    
+    
+        # get the f-part from the linked model record, copy the copy_rec to model-fpart in the simulation DSS
+        for model in model_recs:
+            model_obj = DSS_Tools.organizeLocations(currentAlternative, locations_obj, [model], return_dss_paths=False)
+            dss_model_path, _ = DSS_Tools.getDataLocationDSSInfo(model_obj[0], currentAlternative, computeOptions)
+            model_fpart = dss_model_path.split('/')[6]
+            print('model_fpart: ',model_fpart)
+            DSS_Tools.copy_dss_ts(copy_dss_rec,
+                                  new_fpart=model_fpart,
+                                  dss_file_path=copy_dss_filepath,
+                                  dss_file_alt_outpath=dss_file)
  
     return True
 
